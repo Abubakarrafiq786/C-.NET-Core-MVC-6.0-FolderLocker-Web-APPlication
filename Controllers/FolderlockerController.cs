ecbb0c50505cc9bae1d82a93b0d822961e9b6655
Abubakar Rafiq <152208856+Abubakarrafiq786@users.noreply.github.com>
2023-12-02 00:02:24 +0500

    FolderLocker
    This is a Folder Locker Please Do not be Arrange the all file this is a  C# .Net Core MVC Project ............. If You Have a Any Question Do not be Hasitate...................

---
+++ Controllers/FolderlockerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Security.AccessControl;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Security.Principal;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FolderlockerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LockFolder(string folderPath)
+        {
+            LockUnlockFolder(folderPath, true);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult UnlockFolder(string folderPath)
+        {
+            LockUnlockFolder(folderPath, false);
+            return RedirectToAction("Index");
+        }
+
+        private void LockUnlockFolder(string folderPath, bool isLock)
+        {
+            try
+            {
+                string adminUserName = Environment.UserName;
+                DirectoryInfo dInfo = new DirectoryInfo(folderPath);
+                DirectorySecurity ds = dInfo.GetAccessControl();
+
+                FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+                ds.AddAccessRule(new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny));
+                if (isLock)
+                {
+                    ds.AddAccessRule(fsa);
+                    ViewBag.Status = "Folder Locked";
+                }
+                else
+                {
+                    ds.RemoveAccessRule(fsa);
+                    ViewBag.Status = "Folder Unlocked";
+                }
+
+                dInfo.SetAccessControl(ds);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Status = $"Error: {ex.Message}";
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadFiles(string folderPath, IFormFile file)
+        {
+            UploadFile(folderPath, file);
+            return RedirectToAction("Index");
+        }
+
+        private void UploadFile(string folderPath, IFormFile file)
+        {
+            try
+            {
+                string adminUserName = Environment.UserName;
+                DirectoryInfo dInfo = new DirectoryInfo(folderPath);
+                DirectorySecurity ds = dInfo.GetAccessControl();
+
+                FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+                ds.AddAccessRule(new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny));
+                ds.RemoveAccessRule(fsa);
+                dInfo.SetAccessControl(ds);
+                if (file.Length > 0)
+                {
+                    string filename = Path.Combine(folderPath, file.FileName);
+                    using (var filestream = new FileStream(filename, FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+
+
+                }
+                ds.AddAccessRule(fsa);
+                dInfo.SetAccessControl(ds);
+                ViewBag.Status = "File Uploaded successfully.";
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Status += $"Error uploading file: {ex.Message}";
+            }
+        }
+        public IActionResult delete()
+        {
+            return View();
+        }
+
+
+    }
+
+}
---
+++ Models/UploadModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class UploadModel
+    {
+        public IFormFile Image { get; set; }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public string Description { get; set; }
+    }
+}
